aff125a58c5248c967f5db22caa1b667a7180fce
MarynaIlchenko <debovamail@gmail.com>
2022-02-22 11:19:46 +0200

    taiga-36

--- admin/src/pages/Users.tsx
+++ admin/src/pages/Users.tsx
@@ -1,5 +1,4 @@
 import { Select, Spin, Tag, Typography } from 'antd'
-import axios from 'axios'
 import { useSelector } from 'react-redux'
 import { useEffect, useState } from 'react'
 import AdminLayout from '../components/AdminLayout'
@@ -7,20 +6,31 @@ import { IUser } from '../utils/types'
 import { Table } from 'antd'
 import { ColumnsType } from 'antd/es/table'
 import { Option } from 'antd/lib/mentions'
+import { errorNotification, successNotification } from '../utils/notifications'
+import { privateRoutes } from '../utils/constants'
+import axios, { AxiosError, AxiosResponse } from 'axios'
 
 const { Title } = Typography
 
 function Users() {
     const token = useSelector((state: any) => state.app.token)
     const [users, setUsers] = useState(null)
+    const [tableData, setTableData] = useState([])
+    const [pagination, setPagination] = useState({
+        current: 1,
+        pageSize: 5
+    })
+    const [tableLoading, setTableLoading] = useState(false)
+
+    // const [selectedMenu, setSelectedMenu] = useContext(MenuContext).selectedMenuState
     const dataUsers = [
         {
             id: '111',
             name: 'Name1',
             email: 'debovamail@gmail.com',
+            status: 'admin',
             description: '',
             tags: ['test1', 'test2'],
-            status: 'admin',
             timestamp: 1645440593336
         },
         {
@@ -28,6 +38,8 @@ function Users() {
             name: 'Name2',
             email: 'bbea@com.ua',
             status: 'banned',
+            description: '',
+            tags: ['test1', 'test2'],
             timestamp: 1645440593336
         }
     ]
@@ -51,10 +63,10 @@ function Users() {
             render: (status: string) => {
                 return (
                     <Select defaultValue={status} style={{ width: 120 }}>
-                        <Option value="admin">Admin</Option>
-                        <Option value="moderator">Moderator</Option>
-                        <Option value="banned">Banned</Option>
-                        <Option value="unconfirmed">Unconfirmed</Option>
+                        <Select.Option value="admin">Admin</Select.Option>
+                        <Select.Option value="moderator">Moderator</Select.Option>
+                        <Select.Option value="banned">Banned</Select.Option>
+                        <Select.Option value="unconfirmed">Unconfirmed</Select.Option>
                     </Select>
                 )
             }
@@ -91,10 +103,44 @@ function Users() {
         }
     }, [])
 
+    useEffect(() => {
+        fetchUsers(pagination)
+    }, [])
+
+    function fetchUsers(pagination: any) {
+        setTableLoading(true)
+        axios(privateRoutes.USER, {
+            params: {
+                page: pagination.current,
+                results: pagination.pageSize
+            },
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then((res: AxiosResponse) => {
+                // if (!isMounted) return
+                if (!res.data.meta?.pagination) throw new Error('No pagination obtained')
+                setTableData(res.data.result)
+                setTableLoading(false)
+                setPagination({
+                    ...pagination,
+                    total: res.data.meta.pagination.total
+                })
+            })
+            .catch((err: AxiosError) => errorNotification(err.message))
+    }
     return (
         <AdminLayout currentPage="Users">
             <Title level={1}>Users</Title>
-            <Table dataSource={dataUsers} columns={columnsUsers}></Table>
+            <Table
+                dataSource={tableData}
+                columns={columnsUsers}
+                rowKey={(record: any) => record.id}
+                pagination={pagination}
+                loading={tableLoading}
+                onChange={(pagination: any) => fetchUsers(pagination)}
+            />
         </AdminLayout>
     )
 }
